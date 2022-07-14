a8c6d533c351c8ddcace7828cdf86496cb8d2f5a
Harshit Garg <63019578+harshitgarg595@users.noreply.github.com>
2022-07-14 12:52:12 +0530

    Create ClientApp.java

---
+++ Maven_DemoGit/src/main/java/com/lti/entity/ClientApp.java
@@ -0,0 +1,14 @@
+package com.lti.entity;
+
+public class  ClientApp{
+      public static void main(String[] args) {
+		Calculator c= new  Calculator();
+		System.out.println(c.add(45, 50));
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		
+	}
+}
