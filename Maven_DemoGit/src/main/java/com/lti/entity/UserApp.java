7c93b6a73204a5fd6cd43f55af7c41b53637654c
harshitgarg595 <harshitgarg595@gmail.com>
2022-07-14 12:17:40 +0530

    first commit on Thursday 14 july 2022 created new project

---
+++ Maven_DemoGit/src/main/java/com/lti/entity/UserApp.java
@@ -0,0 +1,10 @@
+package com.lti.entity;
+
+public class UserApp {
+      public static void main(String[] args) {
+		Calculator c= new  Calculator();
+		System.out.println(c.add(45, 50));
+		
+		
+	}
+}
